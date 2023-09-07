610afc112d152c64b4b67c1ad1cfe6aebeb3ac58
tienbot1 <tien.ru@mail.ru>
2023-09-07 21:53:08 +0900

    [AP] add: header components, text components
    test fork and first commit

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import cn from '@/shared/lib/cn/cn';
+import s from '@/app/components/Header/Header.module.scss';
+
+function Header() {
+  return (
+    <h1 className={cn(s.ac__title)}><span>Автоматическое</span> преобразование типов данных</h1>
+  );
+}
+
+export default Header;
---
+++ app/components/Text/Text.tsx
@@ -0,0 +1,10 @@
+import cn from '@/shared/lib/cn/cn';
+import s from '@/app/components/Text/Text.module.scss';
+
+function Header() {
+  return (
+    <p className={cn(s.ac__text)}>В данном случае JavaScript видит, что мы пытаемся выполнить недопустимую для строк, но допустимую для <span>чисел</span> операцию.</p>
+  );
+}
+
+export default Header;
