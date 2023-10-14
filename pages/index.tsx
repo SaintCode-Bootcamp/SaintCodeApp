cee6d88c224d85243137bacd790ac869ee30ba5c
Ksusha <xiuschv@yandex.ru>
2023-10-14 12:37:54 +0300

    add menu for rout "/"

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,16 @@
 import Link from 'next/link'
 import Header from '../app/components/Header'
 import TaskNavigation from '../app/components/TaskNavigation/TaskNavigation'
+import Menu from './menu'
 import styles from '../styles/Header.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.background}>
-      <Header  />
-     
-      <TaskNavigation/>
-    </div>
-  )
+    // <div className={styles.background}>
+    //   <Header  />
+
+    //   <Menu/>
+    // </div>
+    <Menu />
+  );
 }
