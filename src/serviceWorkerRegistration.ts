598e34689ff154a02c0bf55fe768945bd44f9333
Gleb Svechnikov <g.svechnikov@infoculture.com>
2023-10-29 23:01:27 +0400

    Update types

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -17,8 +17,12 @@ const isLocalhost = Boolean(
     // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
+type Config = {
+  onSuccess?: (registration: ServiceWorkerRegistration) => void;
+  onUpdate?: (registration: ServiceWorkerRegistration) => void;
+};
 
-export function register(config) {
+export function register(config?: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
-  base: process.env.APP_ENV === 'production' ? '/' : '/',
+  base: process.env.APP_ENV === 'production' ? '/SaintCodeApp/' : '/',
   build: {
     outDir: './dist',
     emptyOutDir: true,
