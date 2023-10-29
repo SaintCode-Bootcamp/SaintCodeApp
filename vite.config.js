c45450cf9a2c82d0020907d6303335e99d78cd39
Gleb Svechnikov <g.svechnikov@infoculture.com>
2023-10-29 22:03:44 +0400

    Test config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
 export default defineConfig({
   base: process.env.APP_ENV === 'production' ? '/SaintCodeApp/' : '/',
   build: {
