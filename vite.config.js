d054de596c77b55fe38ba0859e8988457e1c7f7f
Gleb Svechnikov <g.svechnikov@infoculture.com>
2023-10-29 16:16:41 +0400

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   plugins: [react()]
 })
