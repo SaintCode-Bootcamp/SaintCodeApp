1ca81579eb7b878a6bae0280b566823c51a3192a
Gleb Svechnikov <g.svechnikov@infoculture.com>
2023-10-29 22:42:54 +0400

    Update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
-  base: process.env.APP_ENV === 'production' ? '/SaintCodeApp/' : '/',
+  base: process.env.APP_ENV === 'production' ? '/' : '/',
   build: {
     outDir: './dist',
     emptyOutDir: true,
